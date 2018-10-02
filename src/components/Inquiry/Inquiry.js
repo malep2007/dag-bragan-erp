e3d32c170f55328f2cd6fe0784a0e9110bab29b2
malep2007 <ephraim.malinga@gmail.com>
2018-10-02 17:59:29 +0300

    #160926221 - added travis file

--- src/components/Inquiry/Inquiry.js
+++ src/components/Inquiry/Inquiry.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import classes from './Inquiry.css'
+import { Link, Route } from 'react-router-dom';
+import CustomerInquiry from './CustomerInquiryForm/CustomerInquiryForm';
 
 const inquiry = () => (
 	<div className={classes.Inquiry}>
 		<h2>Inquiry</h2>
 		<ul>
-			<li>New Inquiry</li>
+			<li>
+				<Link to="/customer/inquiry/new">New Inquiry</Link>
+			</li>
 		</ul>
+		<Route path="/customer/inquiry/new" component={CustomerInquiry}/>
 	</div>
 )
 
